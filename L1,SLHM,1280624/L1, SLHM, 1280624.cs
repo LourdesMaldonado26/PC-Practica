187f668e21c2975de5ab06728783e9076d6dffc4
LourdesMaldonado26 <158303873+LourdesMaldonado26@users.noreply.github.com>
2024-05-22 11:22:21 -0600

    Add files via upload

---
+++ L1,SLHM,1280624/L1, SLHM, 1280624.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Ingrese su nombre: ");
+      string Nombre = Console.ReadLine();
+
+      Console.WriteLine("Hola Mundo");
+      Console.WriteLine("Soy" + "Nombre");
+      
+      /* Comentarios */
+
+      Console.Write("Hola Mundo ");
+      Console.Write("Soy" + "Nombre");
+      Console.ReadKey();
+    }
+    
+    
+}
