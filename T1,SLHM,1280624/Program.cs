42b70f0fedef55463179b4ddcdd287eae7f25dd6
LourdesMaldonado26 <158303873+LourdesMaldonado26@users.noreply.github.com>
2024-05-22 11:19:17 -0600

    Add files via upload

---
+++ T1,SLHM,1280624/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+     static void Main(string[] args)
+ {
+     Console.WriteLine("Mi segundo programa");
+
+     Console.WriteLine("Inrese su nombre");
+     string Nombre = Console.ReadLine();
+     Console.WriteLine("Nombre:" + Nombre);
+
+     Console.WriteLine("Ingrese su edad");
+     string Edad = Console.ReadLine();
+     Console.WriteLine("Edad:" + Edad);
+
+     Console.WriteLine("Ingrese su carrera");
+     string Carrera = Console.ReadLine();
+     Console.WriteLine("Carrera:" + Carrera);
+
+     Console.WriteLine("Ingrese su número de carné");
+     string Carné = Console.ReadLine();
+     Console.WriteLine("Carné:" + Carné);
+
+     Console.ReadKey();
+ }
+}
